e1f797d91e5bba5e9f3459315a4767aeeb344075
artur.blaya <artur.blaya.fernandez@dnv.com>
2024-09-16 19:24:36 +0200

    feat: add basic auth to express ui

--- src/2. Application/file-upload-use-case.ts
+++ src/2. Application/file-upload-use-case.ts
@@ -3,6 +3,7 @@ import { UIInterface } from "./ui-interface";
 export class FileUploadUseCase {
     constructor(private userInterface: UIInterface) {
         userInterface.upload(async (req: any, res: any) => {
+
             return res.status(200).json({
                 message: "File uploaded"
             });
--- src/3. Infrastructure/express-ui.ts
+++ src/3. Infrastructure/express-ui.ts
@@ -1,10 +1,11 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { UIInterface } from '../2. Application/ui-interface';
 
 export class ExpressUi implements UIInterface {
     private app = express();
     constructor() {
         this.app.use(express.json());
+        this.app.use(this.basicAuth);
         this.app.listen(3000, () => { console.log('listening http://localhost:3000') });
     }
     async upload(callback: (req: any, res: any) => Promise<void>): Promise<void> {
@@ -12,5 +13,27 @@ export class ExpressUi implements UIInterface {
             await callback(req, res);
         });
     }
+
+    private basicAuth = (req: any, res: any, next: NextFunction) => {
+        const authHeader = req.headers['authorization'];
+
+        if (!authHeader) {
+            res.setHeader('WWW-Authenticate', 'Basic');
+            return res.status(401).send('Auth required');
+        }
+
+        const base64Credentials = authHeader.split(' ')[1];
+        const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+        const [username, password] = credentials.split(':');
+
+        const USERNAME = 'artur';
+        const PASSWORD = '123456';
+
+        if (username === USERNAME && password === PASSWORD) {
+            return next();
+        }
+
+        return res.status(401).send('Bad credentials');
+    };
 
 }
